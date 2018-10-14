a31e9710ad2de9215b8de29280f51eef9441c046
John Perry Carpio <isystem20@gmail.com>
2018-10-14 15:33:51 +0800

    Edit Gmail Login

--- application/views/UserLogin.php
+++ application/views/UserLogin.php
@@ -1,23 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Mirrored from flatable.phoenixcoded.net/ltr/vertical-static/auth-sign-in-social.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 08 Sep 2018 06:15:42 GMT -->
 <head>
 <title>Flat Able - Premium Admin Template by Phoenixcoded</title>
 
-
-<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-      <![endif]-->
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="description" content="Phoenixcoded">
 <meta name="keywords" content=", Flat ui, Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
-<meta name="author" content="Phoenixcoded">
-
+<!-- <meta name="google-signin-client_id" content="453639693637-ba5cbmedoptto84pnj87k6cnumqls9sv.apps.googleusercontent.com"> -->
+<meta name="google-signin-client_id" content="453639693637-8utkl362om0v1qad2jmgdbbhla6l9m5u.apps.googleusercontent.com">
+<script src="https://apis.google.com/js/platform.js" async defer></script>  
 <link rel="icon" href="<?php echo base_url()?>themes/ltr/vertical-static/assets/images/favicon.ico" type="image/x-icon">
 
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
@@ -59,7 +54,7 @@
 <button class="btn btn-facebook m-b-20"><i class="icofont icofont-social-facebook"></i>Sign in with facebook</button>
 </div>
 <div class="col-md-6">
-<button class="btn btn-twitter m-b-20"><i class="icofont icofont-social-twitter"></i>Sign in with twitter</button>
+<div class="g-signin2" data-onsuccess="onSignIn" data-width="350"></div>
 </div>
 </div>
 <p class="text-inverse b-b-default text-left p-b-5">Sign in with your regular account</p>
@@ -168,7 +163,38 @@
 <script type="text/javascript" src="<?php echo base_url()?>themes/ltr/vertical-static/assets/js/script.js"></script>
 
 <script type="text/javascript" src="<?php echo base_url()?>themes/ltr/vertical-static/assets/js/common-pages.js"></script>
+
+<script type="text/javascript">
+    
+
+
+
+function onSignIn(googleUser) {
+
+    var profile = googleUser.getBasicProfile();
+
+    var id_token = googleUser.getAuthResponse().id_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://acquiretechexam.atwebpages.com/');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    // xhr.onload = function() {
+    //   console.log('Signed in as: ' + xhr.responseText);
+    // };
+    // xhr.send('idtoken=' + id_token);
+    xhr.send('idtoken=' + id_token + '&userid=' + profile.getId() + '&fullname: ' + profile.getName() + '&firstname: ' + profile.getGivenName() + '&lastname: ' + profile.getFamilyName() + "&image: " + profile.getImageUrl() + "&email: " + profile.getEmail());
+    console.log(profile.getImageUrl());
+
+};
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://acquiretechexam.atwebpages.com/";
+    });
+}
+</script>
+
+
 </body>
 
-<!-- Mirrored from flatable.phoenixcoded.net/ltr/vertical-static/auth-sign-in-social.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 08 Sep 2018 06:15:42 GMT -->
 </html>
