76553a0d8fc2a10486a04f43ab93d30914ec1e06
IanNovella1313 <IanNovella1313@users.noreply.github.com>
2018-10-22 15:50:02 +0800

    user masterlist

--- application/views/pages/users/UserMasterlist.php
+++ application/views/pages/users/UserMasterlist.php
@@ -318,16 +318,7 @@
                                                 <td>$313,500</td>
                                             </tr>
                                         </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <th>Name</th>
-                                                <th>Position</th>
-                                                <th>Office</th>
-                                                <th>Age</th>
-                                                <th>Start date</th>
-                                                <th>Salary</th>
-                                            </tr>
-                                        </tfoot>
+                                       
                                     </table>
                                 </div>
                             </div>
