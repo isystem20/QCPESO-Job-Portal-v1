25187934b7b134b1d3c65d7f607aae1f1a293e98
Diego-PC\Diego <diegodillague32996@gmail.com>
2018-10-23 12:38:13 -0700

    Fix table

--- application/views/DisabilitiesMasterlist.php
+++ application/views/DisabilitiesMasterlist.php
@@ -19,11 +19,13 @@
                 </div>
             </div>
 
-<<<<<<< HEAD:application/views/ApplicantDisabilities.php
- <div class="page-body">
+
+ <<div class="page-body">
                 <div class="row">
                     <div class="col-sm-12">
+
                         <div class="card">
+
                             <div class="card-header">
                                 <button class="btn btn-inverse btn-outline-inverse"><i class="icofont icofont-ui-add"></i>Add</button>
                                 <button class="btn btn-inverse btn-outline-inverse"><i class="icofont icofont-edit"></i>Edit</button>
@@ -39,48 +41,21 @@
                                 <div class="dt-responsive table-responsive">
                                     <table id="scr-vtr-dynamic" class="table table-striped table-bordered nowrap">
                                         <thead>
-                                    <tr>
+                                            <tr>
                                                 <th>#</th>
                                                 <th>Name</th>
                                                 <th>Description</th>
                                                 <th>Last Modified By</th>
                                                 <th>Last Modified At</th>
                                                  <th>Status</th>
-=======
-
-            <div class="page-body">
-
-                <div class="card">
-                    <div class="card-header">
-
-                        <div class="card-header-right">
-                            <i class="icofont icofont-rounded-down"></i>
-                            <i class="icofont icofont-refresh"></i>
-                            <i class="icofont icofont-close-circled"></i>
-                        </div>
-                    </div>
-                    <div class="card-block">
-                        <div class="dt-responsive table-responsive">
-                            <table id="scr-vtr-dynamic" class="table table-striped table-bordered nowrap">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Description</th>
-                                        <th>Created By</th>
-                                        <th>Created At</th>
-                                        <th>Modified By</th>
-                                        <th>Modified At</th>
-                                        <th>Active</th>
-                                        <th>Action</th>
->>>>>>> e200200f772c799b6a113abca4bf448205ee72d8:application/views/DisabilitiesMasterlist.php
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php
                                         if ($applicantdisablities->num_rows() > 0) {
                                             foreach ($applicantdisablities->result() as $row) { ?>
-
-                                      <td>
+                                            <tr>
+                                                <td>
                                                     <div class="checkbox-fade fade-in-primary">
                                                     <label>
                                                     <input type="checkbox" id="checkbox" name="Language" value="HTML">
@@ -91,8 +66,7 @@
                                                     </label>
                                                     </div>
                                                 </td>
-                                                
-                                     <td><?php echo $row->name; ?></td>
+                                                <td><?php echo $row->name; ?></td>
                                                 <td><?php echo $row->description; ?></td>
                                                 <td><?php echo $row->modifiedById?></td>
                                                 <td><?php echo $row->modifiedAt?></td>
@@ -111,6 +85,9 @@
                                             }
                                         }
                                         ?>
+
+
+
                                
                             </table>
                         </div>
