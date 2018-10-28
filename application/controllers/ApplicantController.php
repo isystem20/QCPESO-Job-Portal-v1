1631021f8115fefd885e7eb0db8def04a5fb6921
IanNovella1313 <IanNovella1313@users.noreply.github.com>
2018-10-28 16:30:31 +0800

    Applicant Masterlist

--- application/controllers/ApplicantController.php
+++ application/controllers/ApplicantController.php
@@ -20,11 +20,15 @@ class ApplicantController extends CI_Controller {
 	public function ApplicantMasterlist()
 	{
 		$data = array('tables' => TRUE);
+		$this->load->model('ApplicantModel','AppModel');
+		$data['applicants'] = $this->AppModel->LoadApplicantList();
+		// print_r($data);
+		// die();
         $this->load->view('layout/css', $data);
 		$this->load->view('layout/top');
 		$this->load->view('layout/menu-manage');
 		$this->load->view('layout/right');
-		$this->load->view('ApplicantMasterlist');
+		$this->load->view('ApplicantMasterlist',$data);
 		$this->load->view('layout/js', $data);	
 			
 	}
