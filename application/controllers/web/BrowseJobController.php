dd9efb98ebe129e2ec33ed161ea0bfaafb039a53
19TRISKELION68\Nathan <dredbaetiong@gmail.com>
2018-11-02 14:45:17 +0800

    browse job

--- application/controllers/web/BrowseJobController.php
+++ application/controllers/web/BrowseJobController.php
@@ -5,16 +5,14 @@ class BrowseJobController extends CI_Controller {
 
 public function BrowseJob()
 	{
-		
-		$this->load->view('web/BrowseJob');
 		$layout = array('transparentwrapper' => TRUE, );
 		$this->load->view('layout/web/1_head');
 		$this->load->view('layout/web/2_preloader');
 		$this->load->view('layout/web/3_header');
-		$this->load->view('Home');
+		$this->load->view('web/BrowseJob');
 		$this->load->view('layout/web/5_rightbar');
 		$this->load->view('layout/web/6_footer');
-		
+
 	}
 	
 	
