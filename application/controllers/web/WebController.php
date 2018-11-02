d879fb0d44a5e42ff854ab81029d4453f4e03f58
John Perry Carpio <isystem20@gmail.com>
2018-11-02 13:12:25 +0800

    layout for web portal

--- application/controllers/web/WebController.php
+++ application/controllers/web/WebController.php
@@ -3,5 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class WebController extends CI_Controller {
 
+	public function LoadFrontPage() {
+		$this->load->view('layout/web/1_head');
+		$this->load->view('layout/web/2_preloader');
+		$this->load->view('layout/web/3_header');
+		$this->load->view('Home');
+		$this->load->view('layout/web/5_rightbar');
+		$this->load->view('layout/web/6_footer');
+
+	}
 
 }
---
+++ application/views/layout/web/3_header.php
@@ -0,0 +1,36 @@
+
+        <!-- Header-->
+        <header class="header header-transparent">
+            <div class="container-fluid">
+                <!-- Brand-->
+                <div class="inner-header">
+                    <a class="inner-brand" href="#">
+                        <img src="<?php echo base_url();?>themes/ltr/vertical-static/assets/images/auth/qcpeso.png" width="50">
+                        Quezon City PESO
+                    </a>
+                </div>
+                <!-- Navigation-->
+                <div class="inner-navigation collapse">
+                    <div class="inner-nav onepage-nav">
+                        <ul>
+                            <li><a href="#home"><span class="menu-item-span">Home</span></a></li>
+                            <li><a href="#about"><span class="menu-item-span">About</span></a></li>
+                            <li><a href="#services"><span class="menu-item-span">Services</span></a></li>
+                            <li><a href="#jobs"><span class="menu-item-span">Browse Jobs</span></a></li>
+                            <li><a href="#startnow"><span class="menu-item-span">Start Now</span></a></li>
+                            <!-- <li><a href="#team"><span class="menu-item-span">Team</span></a></li> -->
+                            <!-- <li><a href="#blog"><span class="menu-item-span">News</span></a></li> -->
+                            <li><a href="#quick-register"><span class="menu-item-span">Register</span></a></li>
+                            <!-- <li><a href="start.html"><span class="menu-item-span">Demos</span></a></li> -->
+                        </ul>
+                    </div>
+                </div>
+           <!--      <div class="extra-nav">
+                    <ul>
+                        <li><a class="off-canvas-open" href="#"><span class="menu-item-span"><i class="ti-menu"></i></span></a></li>
+                        <li class="nav-toggle"><a href="#" data-toggle="collapse" data-target=".inner-navigation"><span class="menu-item-span"><i class="ti-menu"></i></span></a></li>
+                    </ul>
+                </div> -->
+            </div>
+        </header>
+        <!-- Header end-->
