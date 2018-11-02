8cdee7cef15ec42e7857fb832f6fbfd985499771
John Perry Carpio <isystem20@gmail.com>
2018-11-02 15:24:57 +0800

    header

--- application/controllers/web/ServicesContoller.php
+++ application/controllers/web/ServicesContoller.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ServicesController extends CI_Controller {
 
-public function Service()
+public function Services()
 	{
 		
 		
