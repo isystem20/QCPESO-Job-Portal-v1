4067791b09a2516bbbe723e9d1d193fd6c271349
kurt-PC\kurt <kurtrusselsiggaoat@gmail.com>
2018-11-03 18:44:28 +0800

    license

---
+++ application/models/admin/LanguageModel.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+	class LanguageModel extends CI_Model {
+
+		public function Load_LanguageModel_Masterlist() {
+			$this->db->select('*');
+			$this->db->from('tbl_applicants_lang_read');
+			return $this->db->get();
+		}
+
+	}
