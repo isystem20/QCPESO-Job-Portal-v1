c09141cfe1da15e7689027e141ebeec59d54bc2a
John Perry Carpio <isystem20@gmail.com>
2018-11-03 17:12:15 +0800

    js.php

--- application/views/layout/admin/6_js.php
+++ application/views/layout/admin/6_js.php
@@ -1,35 +1,49 @@
 
     </div>
 
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap popper Core JavaScript -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/bootstrap/js/popper.min.js"></script>
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/perfect-scrollbar.jquery.min.js"></script>
-    <!--Wave Effects -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/sidebarmenu.js"></script>
-    <!--Custom JavaScript -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/custom.min.js"></script>
-    <!-- ============================================================== -->
-    <!-- This page plugins -->
-    <!-- ============================================================== -->
-    <!-- Vector map JavaScript -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-    <!--sparkline JavaScript -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
-    <!--morris JavaScript -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/chartist-js/dist/chartist.min.js"></script>
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
-    <!-- Chart JS -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/dashboard4.js"></script>
-    <!-- ============================================================== -->
-    <!-- Style switcher -->
-    <!-- ============================================================== -->
-    <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+    <?php
+    if (!empty($charts)) { ?>
+
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/jquery/jquery.min.js"></script>
+            <!-- Bootstrap popper Core JavaScript -->
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/bootstrap/js/popper.min.js"></script>
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+            <!-- slimscrollbar scrollbar JavaScript -->
+            <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/perfect-scrollbar.jquery.min.js"></script>
+            <!--Wave Effects -->
+            <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/waves.js"></script>
+            <!--Menu sidebar -->
+            <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/sidebarmenu.js"></script>
+            <!--Custom JavaScript -->
+            <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/custom.min.js"></script>
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+            <!--sparkline JavaScript -->
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+            <!--morris JavaScript -->
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/chartist-js/dist/chartist.min.js"></script>
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
+            <!-- Chart JS -->
+            <script src="<?php echo base_url(); ?>themes/admin-pro/minimal/js/dashboard4.js"></script>
+            <script src="<?php echo base_url(); ?>themes/admin-pro/assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+
+    <?php
+    }
+    ?>
+
+    <?php
+    if (!empty($tables)) { ?>
+
+ 
+        
+    <?php
+    }
+    ?>
+
+
+
+
+
 </body>
 
 </html>
