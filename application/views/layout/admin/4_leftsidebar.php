a6b05e50f9f0a2741cd4f049d1f2e503cda74987
John Perry Carpio <isystem20@gmail.com>
2018-11-03 17:26:04 +0800

    group masterlist

--- application/views/layout/admin/4_leftsidebar.php
+++ application/views/layout/admin/4_leftsidebar.php
@@ -19,9 +19,10 @@
                             </ul>
                         </li>
                         <li class="nav-devider"></li>
-                        <li class="nav-small-cap">PERSONAL</li>
+                        
                         <li> <a class="waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard</span></a>
                         </li>
+                        <li class="nav-small-cap">MANAGE</li>
                         <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-bullseye"></i><span class="hide-menu">Apps</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="app-calendar.html">Calendar</a></li>
