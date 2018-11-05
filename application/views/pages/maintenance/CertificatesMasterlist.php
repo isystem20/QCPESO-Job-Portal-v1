54bf26739c5e4d60bda83bf5c737925c939ef8e4
LAPTOP-4O0R2UIR\Jonathan <jonathanramos751997@gmail.com>
2018-11-05 12:36:49 +0800

    maintennace

--- application/views/pages/maintenance/CertificatesMasterlist.php
+++ application/views/pages/maintenance/CertificatesMasterlist.php
@@ -10,12 +10,12 @@
             <!-- ============================================================== -->
             <div class="row page-titles">
                 <div class="col-md-5 align-self-center">
-                    <h3 class="text-themecolor">Certificates Maintenance</h3>
+                    <h3 class="text-themecolor">Certificates</h3>
                 </div>
                 <div class="col-md-7 align-self-center">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                        <li class="breadcrumb-item">Manage</li>
+                        <li class="breadcrumb-item">Maintenance</li>
                         <li class="breadcrumb-item active">Certificates</li>
 
                     </ol>
